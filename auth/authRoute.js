b53f7de97850f54acbfa1320929ff68f333f64c4
thomasbibi <thomasbibin38@gmail.com>
2025-01-28 20:08:49 +0530

    added authService as param

--- auth/authRoute.js
+++ auth/authRoute.js
@@ -2,11 +2,12 @@ import express from 'express';
 import { reqValidator } from "../middlewares/request/reqValidator.js";
 import { logInController, signUpController } from "./authController.js";
 import { loginValidation, userValidation } from './authValidator.js';
+import * as authServices from './authServices.js';
 
 
 const router = express.Router();
 
-router.post('/user/sign-up', reqValidator(userValidation), signUpController);
-router.post('/user/login', reqValidator(loginValidation), logInController);
+router.post('/user/sign-up', reqValidator(userValidation), signUpController(authServices));
+router.post('/user/login', reqValidator(loginValidation), logInController(authServices));
 
 export default router;
